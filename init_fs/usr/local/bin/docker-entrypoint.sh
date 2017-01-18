429cd25b9de69e8f754197ec0adf2a072517067a
Brendan Jocson <krull.mcroth@gmail.com>
2017-01-18 15:36:56 +0200

    finished zenoss_plvsms for plivo

--- init_fs/usr/local/bin/docker-entrypoint.sh
+++ init_fs/usr/local/bin/docker-entrypoint.sh
@@ -8,5 +8,6 @@ while ! /bin/nc -z zenoss4-mariadb 3306;
     sleep 1;
   done;
 
-echo "Dependencies Started.. Starting the zenoss daemons..."
-service zenoss start & tail -f /usr/local/zenoss/log/Z2.log
+echo "zenoss4-mariadb started! Launching the zenoss daemons..."
+#service zenoss start & tail -f /usr/local/zenoss/log/Z2.log
+service zenoss start & tail -f /dev/null
